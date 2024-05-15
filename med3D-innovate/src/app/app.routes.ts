74c4d984328c82c711f12f826fee7a0beecf916e
ilosvayaron <ilosvayaron@gmail.com>
2024-05-15 12:34:53 +0200

    Project - initial state and basic components

--- med3D-innovate/src/app/app.routes.ts
+++ med3D-innovate/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: "sign-in", component: SignInComponent}
+];
